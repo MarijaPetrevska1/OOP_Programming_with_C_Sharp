5003ad4ad9842fb2e69e578d5635e3279ae5035c
MarijaPetrevska1 <marija_petrevska25@yahoo.com>
2025-03-04 15:09:31 +0100

    Exercise6

---
+++ Class02(data-types,variables,branching)/Homework/Exercise6.1/Exercise6.1/Program.cs
@@ -0,0 +1,52 @@
+﻿Console.WriteLine("======= Exercise 6.1 =======");
+Console.WriteLine("");
+
+// 1. Asking the user to input two numbers
+int num3, num4;
+
+// User Input for the first number
+Console.Write("Enter the first number: ");
+string input1 = Console.ReadLine();
+if (!int.TryParse(input1, out num3))
+{
+    Console.WriteLine("Invalid input for the first number. Please enter a valid integer.");
+    return; 
+}
+
+// User Input for the second number
+Console.Write("Enter the second number: ");
+string input2 = Console.ReadLine();
+if (!int.TryParse(input2, out num4))
+{
+    Console.WriteLine("Invalid input for the second number. Please enter a valid integer.");
+    return; 
+}
+
+// 2. Checking which number is larger
+int largerNumber1;
+if (num3 > num4)
+{
+    largerNumber1 = num3;
+}
+else
+{
+    largerNumber1 = num4;
+}
+
+// 3. Checking if the larger number is even or odd
+string evenOrOdd1;
+if (largerNumber1 % 2 == 0)
+{
+    evenOrOdd1 = "even";
+}
+else
+{
+    evenOrOdd1 = "odd";
+}
+
+// 4. Output the results to the console
+Console.WriteLine("The larger number from the two is " + largerNumber1);
+Console.WriteLine("The number is " + evenOrOdd1);
+
+Console.ReadLine();
+
---
+++ Class02(data-types,variables,branching)/Homework/Exercise6/Exercise6/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("======= Exercise 6 =======");
+Console.WriteLine("");
+
+// 1. Declare and initialize two variables
+int num1 = 10;
+int num2 = 25;
+
+// 2. Checking which number is larger
+int largerNumber;
+if (num1 > num2)
+{
+    largerNumber = num1;
+}
+else
+{
+    largerNumber = num2;
+}
+
+// 3. Checking if the larger number is even or odd
+string evenOrOdd;
+if (largerNumber % 2 == 0)
+{
+    evenOrOdd = "even";
+}
+else
+{
+    evenOrOdd = "odd";
+}
+
+//4. Output the results to the console
+Console.WriteLine("The larger number from the two is " + largerNumber);
+Console.WriteLine("And the number is " + evenOrOdd);
+Console.ReadLine();
+
