20efbbd6e4f16f808b87c5de8dc349d367fe5264
MarijaPetrevska1 <marija_petrevska25@yahoo.com>
2025-03-04 15:17:43 +0100

    Exercise7

--- Class02(data-types,variables,branching)/Homework/Exercise6.1/Exercise6.1/Program.cs
+++ Class02(data-types,variables,branching)/Homework/Exercise6.1/Exercise6.1/Program.cs
@@ -10,7 +10,7 @@ string input1 = Console.ReadLine();
 if (!int.TryParse(input1, out num3))
 {
     Console.WriteLine("Invalid input for the first number. Please enter a valid integer.");
-    return; 
+    return;
 }
 
 // User Input for the second number
@@ -19,34 +19,43 @@ string input2 = Console.ReadLine();
 if (!int.TryParse(input2, out num4))
 {
     Console.WriteLine("Invalid input for the second number. Please enter a valid integer.");
-    return; 
+    return;
 }
 
-// 2. Checking which number is larger
-int largerNumber1;
-if (num3 > num4)
+// 2. Checking if both numbers are the same
+if (num3 == num4)
 {
-    largerNumber1 = num3;
+    Console.WriteLine("Both numbers are the same.");
 }
 else
 {
-    largerNumber1 = num4;
-}
+    // 3. Checking which number is larger
+    int largerNumber1;
+    if (num3 > num4)
+    {
+        largerNumber1 = num3;
+    }
+    else
+    {
+        largerNumber1 = num4;
+    }
 
-// 3. Checking if the larger number is even or odd
-string evenOrOdd1;
-if (largerNumber1 % 2 == 0)
-{
-    evenOrOdd1 = "even";
-}
-else
-{
-    evenOrOdd1 = "odd";
-}
+    // 4. Checking if the larger number is even or odd
+    string evenOrOdd1;
+    if (largerNumber1 % 2 == 0)
+    {
+        evenOrOdd1 = "even";
+    }
+    else
+    {
+        evenOrOdd1 = "odd";
+    }
 
-// 4. Output the results to the console
-Console.WriteLine("The larger number from the two is " + largerNumber1);
-Console.WriteLine("The number is " + evenOrOdd1);
+    // Output the results to the console
+    Console.WriteLine("The larger number from the two is " + largerNumber1);
+    Console.WriteLine("The number is " + evenOrOdd1);
+}
 
 Console.ReadLine();
+
 
--- Class02(data-types,variables,branching)/Homework/Exercise7/Program.cs
+++ Class02(data-types,variables,branching)/Homework/Exercise7/Program.cs
@@ -1,2 +1,37 @@
 ﻿
-Console.WriteLine("Hello, World!");
+Console.WriteLine("======= Exercise 7 =======");
+Console.WriteLine("");
+
+// Asking the user to enter a number between 1 and 3
+Console.Write("Enter a number from 1 to 3: ");
+string input = Console.ReadLine();
+
+// Try to parse the input as an integer
+if (int.TryParse(input, out int number))
+{
+    // Checking the value of the entered number and print the corresponding message
+    if (number == 1)
+    {
+        Console.WriteLine("You got a new car!");
+    }
+    else if (number == 2)
+    {
+        Console.WriteLine("You got a new plane!");
+    }
+    else if (number == 3)
+    {
+        Console.WriteLine("You got a new bike!");
+    }
+    else
+    {
+        // Error message if the number is not between 1 and 3
+        Console.WriteLine("Error: You must enter a number between 1 and 3.");
+    }
+}
+else
+{
+    // Error message if the input is not a valid number
+    Console.WriteLine("Error: Please enter a valid number.");
+}
+
+Console.ReadLine();
