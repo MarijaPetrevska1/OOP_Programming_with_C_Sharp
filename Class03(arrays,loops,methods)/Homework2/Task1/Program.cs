d19a40faf759d7bf6a7393a0615269876f30d436
MarijaPetrevska1 <marija_petrevska25@yahoo.com>
2025-03-09 19:47:34 +0100

    Homework2

---
+++ Class03(arrays,loops,methods)/Homework2/Task1/Program.cs
@@ -0,0 +1,51 @@
+﻿
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("\n============== Homework - TASK 1 ==============\n");
+Console.WriteLine("\nConsole application: SumOfEven\n");
+Console.ResetColor();
+
+//1. Make a console application called SumOfEven.
+//2. Inside it create an array of 6 integers. 
+//3. Get numbers from the input, find and print the sum of the even numbers from the array.
+
+// Creating an array to hold 6 integers
+int[] numbers = new int[6];
+
+// Loop to get input for the array
+for (int i = 0; i < 6; i++)
+{
+    int userInput;
+    bool isValidInput = false;
+
+    // Continue asking until a valid integer is entered
+    while (!isValidInput)
+    {
+        Console.WriteLine($"Enter integer no.{i + 1}:");
+        string input = Console.ReadLine();
+
+        // Try to parse the input to an integer
+        if (int.TryParse(input, out userInput))
+        {
+            numbers[i] = userInput;
+            isValidInput = true; 
+        }
+        else
+        {
+            Console.WriteLine("This is invalid input. Please enter a valid integer!");
+        }
+    }
+}
+
+// The sum of even numbers
+int sum = 0;
+foreach (int number in numbers)
+{
+    if (number % 2 == 0)
+    {
+        sum = sum + number;
+    }
+}
+
+// Display the result
+Console.WriteLine($"The result is: {sum}");
+Console.ReadLine();
---
+++ Task2/Program.cs
@@ -0,0 +1,47 @@
+﻿
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("\n============== Homework - TASK 2 ==============\n");
+Console.WriteLine("\nConsole application: StudentGroup\n");
+Console.ResetColor();
+
+// Steps to do:
+// => Make a new console application called StudentGroup
+// => Make 2 arrays called studentsG1 and studentsG2 and fill them with 5 student names.
+// => Get a number from the console between 1 and 2 and print the students from that group in the console.
+
+// Two arrays with student names
+string[] studentsGroupG1 = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+string[] studentsGroupG2 = { "Marija", "Leonid", "Marko", "Aleksandar", "Ana" };
+
+// Prompt user to enter the student group number (1 or 2)
+Console.Write("Enter student group: (there are 1 and 2) ");
+
+string input = Console.ReadLine();
+int group;
+
+// Checking if the user input is valid 
+while (!int.TryParse(input, out group) || (group != 1 && group != 2))
+{
+    Console.Write("Invalid input. Please enter a valid group number (1 or 2): ");
+    input = Console.ReadLine();
+}
+
+// Display the corresponding students from the selected group
+if (group == 1)
+{
+    Console.WriteLine("The Students in Group G1 are:");
+    for (int i = 0; i < studentsGroupG1.Length; i++)
+    {
+        Console.WriteLine($"{i + 1}. {studentsGroupG1[i]}");
+    }
+}
+else if (group == 2)
+{
+    Console.WriteLine("The Students in Group G2 are:");
+    for (int i = 0; i < studentsGroupG2.Length; i++)
+    {
+        Console.WriteLine($"{i + 1}. {studentsGroupG2[i]}");
+    }
+}
+Console.ReadLine();
+
