0707942f67b31dba21eb179dc8da6eb53e681b4d
MarijaPetrevska1 <marija_petrevska25@yahoo.com>
2025-03-18 20:36:57 +0100

    Class_Code

---
+++ Class05(ClassesAndObjects)/Code/Qinshift.ClassesAndObjects/Models/Person.cs
@@ -0,0 +1,43 @@
+﻿
+namespace Qinshift.ClassesAndObjects.Models
+{
+    public class Person
+    {
+        public Person()
+        {
+            SSN = GenerateSSN();
+        }
+
+   
+        public Person(string fName, string lName, int age)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Age = age;
+            SSN = GenerateSSN();
+        }
+
+
+        
+        public string FirstName { get; set; } = "Martin";
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        private long SSN { get; set; }
+
+        private long GenerateSSN()
+        {
+            return new Random().Next(100000, 999999); 
+        }
+
+        public long GetSSN()
+        {
+            return SSN;
+        }
+
+        
+        public void Talk(string text)
+        {
+            Console.WriteLine($"The human named {FirstName} is saying {text}!");
+        }
+    }
+}
---
+++ Class05(ClassesAndObjects)/Code/Qinshift.ClassesAndObjects/Program.cs
@@ -0,0 +1,37 @@
+﻿using Qinshift.ClassesAndObjects.Models;
+using System;
+
+
+#region Simple class definition and usage
+
+User user = new User();
+user.UserName = "martinp";
+user.Password = "12345";
+Console.WriteLine(user.UserName);
+
+#endregion
+
+
+#region Classes and Objects
+
+Person bob = new Person();
+bob.FirstName = "Bob";
+bob.LastName = "Bobsky";
+bob.Age = 30;
+bob.Talk("Hey there! We are learning classes and objects!");
+
+
+Person jill = new Person("Jill", "Wayne", 35);
+jill.Talk("Hi there!");
+Console.WriteLine(jill.GetSSN());
+
+
+Person martin = new()
+{
+    FirstName = "Martin",
+    LastName = "Panovski",
+    Age = 31,
+};
+
+
+#endregion
